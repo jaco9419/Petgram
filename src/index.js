f0363d06bc1d5523081806758839d89cae38e40e
jaco9419 <jaco.9419@gmail.com>
2023-03-06 10:54:16 -0300

    6-Linter setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<h1>'hola 18'</h1>);
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('app')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(<h1>'hola 18'</h1>)
